db9074d889a2024a1243d592411a3078b68b5ff7
twinklelahariya1 <twinklelahariya1@github.com>
2023-07-27 19:29:45 +0100

    Static data rendering using typescript and react for the star game

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const stars = 9;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          {utils.range(1, stars).map((starId) => (
+            <div key={starId} className="star" />
+          ))}
+        </div>
+        <div className="right">
+          {utils.range(1, 9).map((number) => (
+            <button key={number} className="number">
+              {number}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
     </div>
   );
 }
 
 export default App;
+
+// Color Theme
+const colors = {
+  available: "lightgray",
+  used: "lightgreen",
+  wrong: "lightcoral",
+  candidate: "deepskyblue",
+};
+
+// Math science
+const utils = {
+  // Sum an array
+  sum: (arr: number[]) =>
+    arr.reduce((acc: number, curr: number) => acc + curr, 0),
+
+  // create an array of numbers between min and max (edges included)
+  range: (min: number, max: number) =>
+    Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+  // pick a random number between min and max (edges included)
+  random: (min: number, max: number) =>
+    min + Math.floor(Math.random() * (max - min + 1)),
+
+  // Given an array of numbers and a max...
+  // Pick a random sum (< max) from the set of all available sums in arr
+  randomSumIn: (arr: number[], max: number) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        console.log(arr[i]);
+        const candidateSet = sets[j].concat(arr[i] as any);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length - 1)];
+  },
+};
